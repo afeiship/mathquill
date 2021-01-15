719cb940f2672b9a2344f975ff80c590412d7692
afeiship <1290657123@qq.com>
2021-01-15 18:33:58 +0800

    feat: test  @edtr-io/mathquill

--- __tests__/debug.js
+++ __tests__/debug.js
@@ -1,8 +1,17 @@
-const MathQuill = require('../src/mathquill')
+// const MathQuill = require('../src/mathquill')
+const global = typeof window !== 'undefined' ? window : this || Function('return this')();
 const jquery = require('jquery');
 
-this.jquery = jQuery;
+
+console.log(jquery);
+global.jQuery = jquery;
+
+console.log(global.jQuery);
+
+const MathQuill2 = require('@edtr-io/mathquill/build/mathquill');
+
+// console.log(jquery);
 
 
 
-console.log(MathQuill);
+// console.log(MathQuill2);
