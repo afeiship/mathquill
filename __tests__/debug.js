1db384946cba71d7b57fff6a32212c5fd1ff1e67
afeiship <1290657123@qq.com>
2021-01-15 18:39:23 +0800

    wip

--- __tests__/debug.js
+++ __tests__/debug.js
@@ -1,12 +1,5 @@
 // const MathQuill = require('../src/mathquill')
 const global = typeof window !== 'undefined' ? window : this || Function('return this')();
-const jquery = require('jquery');
-
-
-console.log(jquery);
-global.jQuery = jquery;
-
-console.log(global.jQuery);
 
 const MathQuill2 = require('@edtr-io/mathquill/build/mathquill');
 
